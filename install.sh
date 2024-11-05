334ca4308786825582831f565ca0704d30b3bba3
Jules LOUAPRE <75941534+Jxlio@users.noreply.github.com>
2024-11-05 17:50:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Script to Install and deploy the web proxy 
+sudo apt install golang-go
+go mod init main 
+go mod tidy
+go build main.go utils.go proxy.go detection.go struct.go -o idefix-proxy
+docker pull redis
+docker run -d --name redis-container -p 127.0.0.1:6379:6379 redis
+openssl req -x509 -newkey rsa:2048 -nodes -keyout server.key -out server.crt -days 365
+echo "Successfully installed the web proxy"
+echo "To run the proxy, execute sudo ./idefix-proxy"
