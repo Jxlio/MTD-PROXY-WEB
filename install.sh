5f9d2bc12458345c752b9d97136cbe0aa8fd67e1
Jules LOUAPRE <75941534+Jxlio@users.noreply.github.com>
2024-11-05 18:06:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 sudo apt install golang-go
 go mod init main 
 go mod tidy
-go build main.go utils.go proxy.go detection.go struct.go -o idefix-proxy
+go build main.go utils.go proxy.go detection.go struct.go 
+sudo mv main idefix-proxy
 docker pull redis
 docker run -d --name redis-container -p 127.0.0.1:6379:6379 redis
-openssl req -x509 -newkey rsa:2048 -nodes -keyout server.key -out server.crt -days 365
+openssl req -x509 -newkey rsa:2048 -nodes -keyout server.key -out server.crt -days 365 -subj "/CN=localhost"
 echo "Successfully installed the web proxy"
 echo "To run the proxy, execute sudo ./idefix-proxy"
